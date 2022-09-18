8e041e514032994505c58fc850c62ad9edc47614
cevolkan-ozturk <98951889+cevolkan-ozturk@users.noreply.github.com>
2022-09-18 10:22:53 +0300

    first commit

---
+++ src/com/javastream/GroupBy.java
@@ -0,0 +1,15 @@
+package com.javastream;
+
+import java.util.List;
+import java.util.Map;
+
+public class GroupBy {
+
+    public static Map<String, List<Person>> groupByNationality (String nationality){
+        
+        Map<String,List<Person>> listMap = null;
+        
+        return  listMap;
+    }
+        
+}
---
+++ src/com/javastream/Person.java
@@ -0,0 +1,20 @@
+package com.javastream;
+
+public class Person {
+
+    private String name ;
+    private int age ;
+    private String nationality ;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+}
